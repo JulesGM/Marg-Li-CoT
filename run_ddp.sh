e98ffbd01e95809eed94a776954310255402a2b3
Jules Gagnon-Marchand <julesgm@beluga2.int.ets1.calculquebec.ca>
2022-08-25 21:10:09 -0400

    ddp

--- run_ddp.sh
+++ run_ddp.sh
@@ -1,1 +1,2 @@
-WANDB_MODE=dryrun python bin_refine.py --wandb_run_id=None --distribute_strategy=ddp
+#/usr/bin/env bash
+srun bash -c 'WANDB_MODE=dryrun python bin_refine.py --wandb_run_id=None --distribute_strategy=ddp'
