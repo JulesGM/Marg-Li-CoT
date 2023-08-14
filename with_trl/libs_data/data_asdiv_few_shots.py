b34240018650346813d203c1165f6b55367ab4e7
Jules Gagnon-Marchand <jules.gagnonm.alt@gmail.com>
2023-08-14 19:47:07 -0400

    data fix

---
+++ with_trl/libs_data/data_asdiv_few_shots.py
@@ -0,0 +1,31 @@
+FEW_SHOTS = """
+Q: Seven red apples and two green apples are in the basket. How many apples are in the basket?
+A: 7+2=9
+
+Q: Ellen has six more balls than Marin. Marin has nine balls. How many balls does Ellen have?
+A: 6+9=15
+
+Q: Janet has nine oranges and Sharon has seven oranges. How many oranges do Janet and Sharon have together? 
+A: 9+7=16
+
+Q: Allan brought two balloons and Jake brought four balloons to the park. How many balloons did Allan and Jake have in the park?
+A: 2+4=6
+
+Q: Adam has five more apples than Jackie. Jackie has nine apples. How many apples does Adam have?
+A: 5+9=14
+
+Q: Brian has four more plums than Paul. Paul has seven plums. How many plums does Brian have?
+A: 4+7=11
+
+Q: Sandra took six cups of coffee and Marcie took two cups of coffee. How many cups of coffee did Sandra and Marcie take in total?
+A: 2+6=8
+
+Q: 17 plums were in the basket. More plums were added to the basket. Now there are 21 plums. How many plums were added to the basket?
+A: 21-17=4
+
+Q: 14 red plums and four green plums are in the basket. How many plums are in the basket? 
+A: 4+14=18
+
+Q: Marin has nine apples and Donald has two apples. How many apples do Marin and Donald have together?
+A: 9+2=11
+"""
---
+++ with_trl/libs_data/data_commonsense_qa_few_shot.py
@@ -0,0 +1,95 @@
+# This is ok because it's tiny, < 100 lines
+
+FEW_SHOT = """
+Q: Why do I eat just enough when eating breakfast?
+Answer Choices:
+(A) gas
+(B) full stomach
+(C) feeling satisfied
+(D) have energy
+(E) bloated
+A: The answer must be a reason for eating just enough when eating breakfast. Eating just enough is a way to feel satisfied. Therefore, the answer is feeling satisfied (C).
+
+Q: How is a dog likely to communicate with another dog?
+Answer Choices:
+(A) reproduce
+(B) bark
+(C) do many things
+(D) clone
+(E) jump up
+A: The answer must be a way for a dog to communicate with another dog. Dogs bark to communicate with each other. Therefore, the answer is bark (B).
+
+Q: If a person is trying to weasel out of punishment for a crime, where are they likely to be?
+Answer Choices:
+(A) court room
+(B) cherry tree
+(C) chicken coop
+(D) natural history museum
+(E) jail
+A: The answer must be a place where a person would be trying to get out of punishment for a crime. The answer is court room (A).
+
+Q: What would easily hide a snake?
+Answer Choices:
+(A) living room of a house
+(B) kew gardens
+(C) terrarium
+(D) thick forest
+(E) tropical forest
+A: The answer must be something that would hide a snake. The answer is thick forest (D).
+
+Q: The person didn’t like the varying size of the cake slices handed out, she demand more what?
+Answer Choices:
+(A) fairness
+(B) candies
+(C) compliments
+(D) well fed
+(E) arguements 2
+A: The answer must be a reason for the person to demand more cake. The person is not happy with the size of the cake slices. Therefore, the answer is fairness (A).
+
+Q: In America, the courthouse of any given town has a likely location, where is it?
+Answer Choices:
+(A) electrical circuit
+(B) getting married
+(C) capital city
+(D) center of town
+(E) michigan
+A: The answer must be a location where a courthouse is located. The capital city is the location of the capital of a state. Therefore, the answer is center of town (D).
+
+Q: Where might I find a bowl nestled among other bowls in the kitchen?
+Answer Choices:
+(A) refrigerator
+(B) kitchen top
+(C) cupboard
+(D) dishwasher
+(E) apartment
+A: The answer must be a place where a bowl is found. The kitchen is a place where bowls are found. Therefore, the answer is cupboard (C).
+
+Q: Which state has blue ridge mountains that are called Blue Mountain?
+Answer Choices:
+(A) appalachians
+(B) virginia
+(C) pennsylvania
+(D) north carolina
+(E) montana 
+A: The answer must be a state with mountains. The answer is Pennsylvania (C).
+
+Q: Where could you find many radio studio?
+Answer Choices:
+(A) radio station
+(B) country
+(C) clear area
+(D) office building
+(E) large city
+A: The answer must be a place where many radio studios are located. Radio studios are used to broadcast radio programs. Therefore, the answer is large city (E).
+
+Q: Where would someone bring you a cup? 
+Answer Choices: 
+(A) apartment
+(B) closet
+(C) restaurant
+(D) table
+(E) party
+A: The answer must be a place where someone would bring you a cup. A restaurant is a place where people bring cups. Therefore, the answer is restaurant (C).
+"""
+
+
